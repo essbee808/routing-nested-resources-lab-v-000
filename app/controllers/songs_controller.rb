0d64081eb41e793344d2365d7ea38f72e5e0460d
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-27 05:05:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
-        redirect_to artists_path
+        redirect_to artists_path, alert: "Artist not found."
         flash[:alert] = "Artist not found."
         @songs = @artist.songs
       else
